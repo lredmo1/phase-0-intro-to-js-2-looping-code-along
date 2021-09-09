5ea85adc3e6918a37d30aa4885e8b6f1efacd350
Lisa <redlisa125@gmail.com>
2021-09-08 18:05:47 -1000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+const names = ["Lisa", "Mark", "Jay", "Bey"];
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let n = 0; n < names.length; n++) {
+        messages.push(`Thank you, ${names[n]}, for the wonderful surprise gift!`)
+    }
+    console.log(messages)
+    return messages;
+};
+
+writeCards(names);
+
+function countDown(number) {
+    while (number > -1) {
+        console.log(number);
+        number--
+    }
+};
+
+countDown(10);
