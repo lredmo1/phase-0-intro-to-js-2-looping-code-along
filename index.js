58b6978be2f52a0b3af7e5ebd6dd04f129f0ddfd
Lisa <redlisa125@gmail.com>
2021-09-23 07:38:49 -1000

    commit tree aed3c5b74f841b7ebca753638a18997aa1524548

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-const gifts = ["teddy bear", "drone", "doll"];
+// const gifts = ["teddy bear", "drone", "doll"];
 
-function wrapGifts(gifts) {
-    for (let i = 0; i < gifts.length; i++) {
-        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+// function wrapGifts(gifts) {
+//     for (let i = 0; i < gifts.length; i++) {
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+// const names = ["Lisa", "Mark", "Jay", "Bey"];
+
+// function writeCards(names, event) {
+//     let messages = [];
+//     for (let n = 0; n < names.length; n++) {
+//         messages.push(`Thank you, ${names[n]}, for the wonderful surprise gift!`)
+//     }
+//     console.log(messages)
+//     return messages;
+// };
+
+// writeCards(names);
+
+// function countDown(number) {
+//     while (number > -1) {
+//         console.log(number);
+//         number--
+//     }
+// };
+
+// countDown(10);
+
+function writeCards(namesArray, eventName) {
+    let newArray = [];
+    for (i = 0; i < namesArray.length; i++) {
+        newArray.push(`Thank you ${namesArray[i]} for my ${eventName} gift!`);
     }
-
-    return gifts;
+    return newArray;
 }
 
-wrapGifts(gifts);
-
-
-const names = ["Lisa", "Mark", "Jay", "Bey"];
-
-function writeCards(names, event) {
-    let messages = [];
-    for (let n = 0; n < names.length; n++) {
-        messages.push(`Thank you, ${names[n]}, for the wonderful surprise gift!`)
-    }
-    console.log(messages)
-    return messages;
-};
-
-writeCards(names);
-
-function countDown(number) {
-    while (number > -1) {
-        console.log(number);
-        number--
-    }
-};
-
-countDown(10);
+// function writeCards(names, event) {
+//     let messages = [];
+//     for (let n = 0; n < names.length; n++) {
+//         messages.push(`Thank you, ${names[n]}, for the wonderful surprise gift!`)
+//     }
+//     console.log(messages)
+//     return messages;
+// };
